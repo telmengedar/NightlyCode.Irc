9543cde60cb630319f96b12f3e59e9d0c3350409
Toni Gottschall <Toni.Gottschall@nightlycode.de>
2018-05-29 07:52:56 +0200

    fixed error when command without arguments was parsed

--- IRC/IrcParser.cs
+++ IRC/IrcParser.cs
@@ -66,7 +66,7 @@ namespace NightlyCode.IRC {
                 yield return messagetext.Substring(offset, nextindex - offset);
                 offset = nextindex + 1;
             }
-        } 
+        }
 
         /// <summary>
         /// parses an <see cref="IrcMessage"/> from a string
@@ -80,23 +80,29 @@ namespace NightlyCode.IRC {
             int offset = 0;
             int nextindex = 0;
             IrcMessage message = new IrcMessage();
-            if(messagetext[0]=='@') {
+            if(messagetext[0] == '@') {
                 nextindex = messagetext.IndexOf(' ');
-                message.Tags = messagetext.Substring(1, nextindex-1).Split(';').Select(ParseTag).ToArray();
+                message.Tags = messagetext.Substring(1, nextindex - 1).Split(';').Select(ParseTag).ToArray();
                 offset = nextindex + 1;
             }
 
             if(messagetext[offset] == ':') {
                 nextindex = messagetext.IndexOf(' ', offset);
-                message.Source = messagetext.Substring(offset+1, nextindex - offset-1);
+                message.Source = messagetext.Substring(offset + 1, nextindex - offset - 1);
                 offset = nextindex + 1;
             }
 
             nextindex = messagetext.IndexOf(' ', offset);
-            message.Command = messagetext.Substring(offset, nextindex - offset);
-            offset = nextindex + 1;
+            if(nextindex == -1) {
+                message.Command = messagetext;
+                message.Arguments = new string[0];
+            }
+            else {
+                message.Command = messagetext.Substring(offset, nextindex - offset);
+                offset = nextindex + 1;
 
-            message.Arguments = ParseArguments(messagetext, offset).ToArray();
+                message.Arguments = ParseArguments(messagetext, offset).ToArray();
+            }
             return message;
         }
     }
